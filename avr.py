0b5db734a68f5bef0936cc7e3ca1efa8d5e8427d
Glitch (glitch-hello-website) <none>
2023-06-26 19:28:24 +0000

    🚧🍐 Checkpoint ./templates/index.html:57629255/4553 ./.glitch-assets:57629255/1088 ./avr.py:57629255/291 ./templates/avr1.html:57629255/1641

--- avr.py
+++ avr.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def avr():
-        return render_template('avr.html')
+        return render_template('index.html')
+  
+  @app.route('/avr1')
+def avr1():
+        return render_template('avr1.html')
+  
+  @app.route('/avr2')
+def avr2():
+        return render_template('avr2.html')
+  
+  @app.route('/avr3')
+def avr3():
+        return render_template('avr3.html')
 
 if __name__ == '__main__':
     app.run()
