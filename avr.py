9dfc3a0f4444a4fe3af9b2ce543b4ca61918f5a0
Glitch (nielitavr) <none>
2023-06-27 04:45:40 +0000

    Rewound to commit 79213964ffc1f2921fd3b10221e1d8e9ec937580 + 0 edits

---
+++ avr.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def avr():
+    return render_template('index.html')
+
+@app.route('/avr1')
+def avr1():
+    return render_template('avr1.html')
+
+@app.route('/avr2')
+def avr2():
+    return render_template('avr2.html')
+
+@app.route('/avr3')
+def avr3():
+    return render_template('avr3.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ start.sh
@@ -0,0 +1,1 @@
+python avr.py
